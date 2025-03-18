520c0481e71a6e52904b221840599f461545f4a0
elhalili <amine.elhalili1@gmail.com>
2025-03-18 16:47:46 +0000

    The type builtin: builtins

--- app/main.ts
+++ app/main.ts
@@ -2,12 +2,14 @@ import assert from "assert";
 import { exit } from "process";
 import { createInterface } from "readline/promises";
 
+const shell_builtin = ['echo', 'exit', 'type'];
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 async function main() {
+
   while (true) {
     const answer = await rl.question('$ ');
     const cmd = answer.trim().split(/\s+/);
@@ -16,20 +18,34 @@ async function main() {
       assert(false, "Must provide at least one command");
     }
 
-    if (cmd[0] === 'exit') {
+    if (!shell_builtin.includes(cmd[0])) {
+      rl.write(`${answer}: command not found\n`);
+      continue;
+    }
+    else if (cmd[0] === 'type') {
+      if (cmd.length < 2) {
+        assert(false, 'Not implemented yet');
+      }
+      
+      if (!shell_builtin.includes(cmd[1])) {
+        rl.write(`${cmd[1]}: command not found\n`);
+        continue;
+      }
+
+      rl.write(`${cmd[1]} is a shell builtin\n`)
+      continue;
+    }
+    else if (cmd[0] === 'echo') {
+      cmd.slice(1).forEach(e => rl.write(`${e} `));
+      rl.write('\n');
+      continue;
+    }
+    else if (cmd[0] === 'exit') {
       let num = Number.parseInt(cmd[1]);
       if (!isNaN(num)) {
         exit(num);
       }
     }
-
-    if (cmd[0] === 'echo') {
-      cmd.slice(1).forEach(e => rl.write(`${e} `));
-      rl.write('\n');
-      continue;
-    }
-
-    rl.write(`${answer}: command not found\n`);
   }
 }
 
