45fbc1d34c755806013a2d677f7621f4956cb934
elhalili <amine.elhalili1@gmail.com>
2025-03-18 16:22:55 +0000

    The exit builtin

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import assert from "assert";
+import { exit } from "process";
 import { createInterface } from "readline/promises";
 
 const rl = createInterface({
@@ -8,6 +10,19 @@ const rl = createInterface({
 async function main() {
   while (true) {
     const answer = await rl.question('$ ');
+    const cmd = answer.trim().split(/\s+/);
+    
+    if (cmd.length === 0) {
+      assert(false, "Must provide at least one command");
+    }
+
+    if (cmd[0] === 'exit') {
+      let num = Number.parseInt(cmd[1]);
+      if (!isNaN(num)) {
+        exit(num);
+      }
+    }
+
     rl.write(`${answer}: command not found\n`);
   }
 }
